22ce179379db33a628f948ee25ac71c58c624731
pkametan0502-tech <pkametan0502@gmail.com>
2026-03-31 20:42:03 +1000

    Push Firebase config file for project

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBK5IL4aVisr_SiRF2P7lxkYbDPSGZOBLs",
+  authDomain: "nac-airside-operations.firebaseapp.com",
+  projectId: "nac-airside-operations",
+  storageBucket: "nac-airside-operations.firebasestorage.app",
+  messagingSenderId: "78741928497",
+  appId: "1:78741928497:web:c4ddbd0ca1b0c628583310",
+  measurementId: "G-6RE16S88TR"
+};
+
+export default firebaseConfig;
